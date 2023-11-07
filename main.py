b056364c8438ae7748a495c616bba66db1a0540f
firelop <49453048+firelop@users.noreply.github.com>
2023-11-07 14:34:44 +0100

    Eventually ?

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+monitorInfo = pygame.display.Info()  # Get the resolution of the screen
+screen = pygame.display.set_mode((monitorInfo.current_w*.93, monitorInfo.current_h*.93))
+pygame.display.set_caption("NyanBomb")  # Set the window title
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    dt = clock.tick(60) / 1000  # Represent the time spent since the last frame
+
+pygame.quit()
