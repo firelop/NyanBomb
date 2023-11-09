29ebf08fdc4a8b1c3e518cf7c415f5c3784ab811
novelty <nathanael.queraud@saintaubinlasalle.fr>
2023-11-09 09:54:04 +0100

    class firework render method finished

--- fireworks.py
+++ fireworks.py
@@ -1,5 +1,6 @@
 # Modules nécessaires
-import pygame
+import pygame, math
+
 
 class Particle:
     '''
@@ -7,21 +8,22 @@ class Particle:
     
     S'initialise lors de la création d'un feu d'artifice.
     '''
-    def __init__(self, x, y) -> None:
+    def __init__(self, x, y, screen, accelX, accelY=0) -> None:
         '''
         Initialisation d'une instance Particle
         :arg x, float, coordonnée x de la position de la particule
         :arg y, float, coordonnée y de la position de la particule
         '''
+        self.accelX = accelX
+        self.accelY = accelY
         self.x = x
         self.y = y
-        self.display()
+        self.screen = screen
     
-    def display(self, screen):
-        pass
-
-    def update(self):
-        pass
+    def render(self):
+        self.x += self.accelX
+        self.y += self.accelY
+        pygame.draw.circle(self.screen, "red", (self.x, self.y), 10)
 
 class Firework:
     '''
@@ -29,17 +31,19 @@ class Firework:
     
     la classe s'initialise lors du clic par le client
     '''
-    def __init__(self, x, y) -> None:
+    def __init__(self, x, y, screen) -> None:
         '''
         Initialisation d'une instance Firework
         :arg x, float, coordonnée x de la position de la particule
         :arg y, float, coordonnée y de la position de la particule
         '''
+        particleAccelX = [2*j-j for j in range(8)]
+        self.screen = screen
         self.particles = []
         for i in range(8):
-            self.particles.append(Particle(x, y))
-    
+            self.particles.append(Particle(x, y, self .screen, math.sin(math.radians(i*(360/8)))))
+
     def update(self):
         '''Met à jour les particules'''
         for particle in self.particles:
-            particle.update()
+            particle.render()
